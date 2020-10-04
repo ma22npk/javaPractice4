a743667b587ba2fb0ad5c12d9cc3df4224e2620c
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 05:49:42 +0900

    this

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ class Main {
   public static void main(String[] args) {
     // Personクラスのインスタンスを生成し、変数person1に代入してください
     Person person1 = new Person();
-    person1.hello();
     person1.name = "Kate Jones";
     System.out.println(person1.name);
+    person1.hello();
     // Personクラスのインスタンスを生成し、変数person2に代入してください
     Person person2 = new Person();
-    person2.hello();
     person2.name = "John Christopher Smith";
     System.out.println(person2.name);
+    person2.hello();
   }
 }
