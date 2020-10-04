41225390fdf2f051643ddf972e9eb654e909cf81
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 05:39:47 +0900

    インスタンスフィールドを定義

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 class Main {
-// Personクラスを定義して、インスタンスを生成してみます。
+  // Personクラスを定義して、インスタンスを生成してみます。
+  // インスタンスメソッドは、インスタンスのメソッドです。クラスに定義しても、実際は各インスタンスに属するというイメージを持っておきましょう。
+  // それゆえ、インスタンスメソッドはインスタンス（を代入した変数）に対して呼び出します。
   public static void main(String[] args) {
     // Personクラスのインスタンスを生成し、変数person1に代入してください
     Person person1 = new Person();
-
+    person1.hello();
+    person1.name = "Kate Jones";
+    System.out.println(person1.name);
     // Personクラスのインスタンスを生成し、変数person2に代入してください
     Person person2 = new Person();
+    person2.hello();
+    person2.name = "John Christopher Smith";
+    System.out.println(person2.name);
   }
 }
